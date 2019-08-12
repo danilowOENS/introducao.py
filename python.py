3ca61955fc08d4538bfbcff10fee52fd3e727be0
ubuntu <dl539994@gmail.com>
2019-08-12 18:00:51 -0300

    criar sistema basico de nota

--- python.py
+++ python.py
@@ -1,1 +1,18 @@
-ingles_n6
+import statistics as es 
+
+ingles_n1 = float(input('digite a primeira nota: '))
+ingles_n2 = float(input('digite a segunda nota: '))
+
+lista_notas = [ingles_n1, ingles_n2 ]
+
+soma = sum(lista_notas)
+media = es.mean(lista_notas)
+ 
+if media >= 7:
+     print('aprovado')
+
+elif media >= 5 and media < 7:
+    print('Recuperacao')
+
+else:
+    print('Reprovado')
